d0fa998ddc21c186abf0b4bae34f7221f2f35b72
Lorenzo Civolani <lorenzo.civolani@unibo.it>
2021-12-05 12:10:26 +0100

    day 05 part 1: minor changes

--- day05p1/main.go
+++ day05p1/main.go
@@ -18,23 +18,21 @@ func main() {
 	for scan.Scan() {
 		var from, to coord
 		fmt.Sscanf(scan.Text(), "%d,%d -> %d,%d", &from.x, &from.y, &to.x, &to.y)
-		//fmt.Println(from, to)
+
 		if from.x == to.x {
-			//fmt.Println("horizontal")
+			// horizontal
 			if from.y > to.y {
 				from, to = to, from
 			}
 			for y := from.y; y <= to.y; y++ {
-				//fmt.Println(coord{from.x, y})
 				overlaps[coord{from.x, y}]++
 			}
 		} else if from.y == to.y {
-			//fmt.Println("vertical")
+			// vertical
 			if from.x > to.x {
 				from, to = to, from
 			}
 			for x := from.x; x <= to.x; x++ {
-				//fmt.Println(coord{x, from.y})
 				overlaps[coord{x, from.y}]++
 			}
 		}
