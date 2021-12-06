97162990299d829738d9919c428515dd2bde01ef
Lorenzo Civolani <lorenzo.civolani@unibo.it>
2021-12-06 14:50:31 +0100

    day 06 part 2: add cli options

--- day06p2/main.go
+++ day06p2/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -9,26 +10,28 @@ import (
 	"strings"
 )
 
-const nDays = 256
+var days = flag.Int("days", 256, "number of days to simulate")
 
 func main() {
+	flag.Parse()
+
 	buf, err := io.ReadAll(os.Stdin)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// [timer value] -> # of fishes
 	var fishes [9]int
 	for _, s := range strings.Split(string(buf), ",") {
 		n, err := strconv.Atoi(s)
 		if err != nil {
 			log.Fatal(err)
 		}
-
 		fishes[n]++
 	}
 
 	//fmt.Println("Initial state:", fishes)
-	for day := 0; day < nDays; day++ {
+	for day := 0; day < *days; day++ {
 		creators := fishes[0]
 		copy(fishes[:], fishes[1:])
 
