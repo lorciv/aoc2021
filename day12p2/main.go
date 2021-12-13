b2d491b1093a3e9ecd68081c83f8573f207e8e72
Lorenzo Civolani <lorenzo.civolani@unibo.it>
2021-12-13 23:59:47 +0100

    day 12 part 2

---
+++ day12p2/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	neighbors := make(map[string][]string)
+
+	scan := bufio.NewScanner(os.Stdin)
+	for scan.Scan() {
+		s := strings.Split(scan.Text(), "-")
+		neighbors[s[0]] = append(neighbors[s[0]], s[1])
+		neighbors[s[1]] = append(neighbors[s[1]], s[0])
+	}
+	if err := scan.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	var paths [][]string
+
+	queue := [][]string{
+		{"start"},
+	}
+	for len(queue) > 0 {
+		path := queue[0]
+		queue = queue[1:]
+
+		node := path[len(path)-1]
+
+		if node == "end" {
+			paths = append(paths, path)
+			continue
+		}
+
+		for _, neigh := range neighbors[node] {
+			// Can only visit a single small cave twice (except for start)
+			if neigh == "start" {
+				continue
+			}
+
+			twice := false
+			for i := 0; i < len(path); i++ {
+				for j := 0; j < len(path); j++ {
+					if i == j {
+						continue
+					}
+					if strings.ToLower(path[i]) != path[i] || strings.ToLower(path[j]) != path[j] {
+						continue
+					}
+					if path[i] == path[j] {
+						twice = true
+						break
+					}
+				}
+				if twice {
+					break
+				}
+			}
+
+			visited := false
+			for _, v := range path {
+				if strings.ToLower(v) == v && neigh == v {
+					visited = true
+					break
+				}
+			}
+			if visited && twice {
+				continue
+			}
+			if visited {
+				twice = true
+			}
+
+			newPath := make([]string, len(path), len(path)+1)
+			copy(newPath, path)
+			newPath = append(newPath, neigh)
+
+			queue = append(queue, newPath)
+		}
+	}
+
+	fmt.Println(len(paths))
+}
