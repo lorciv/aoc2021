b4474ee96f9d9acc944757bb3212785235de8149
Lorenzo Civolani <lorenzo.civolani@unibo.it>
2022-01-05 22:54:01 +0100

    day 15 part 1

---
+++ day15p1/main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var (
+	risk        map[coord]int
+	best        map[coord]int
+	rows, cols  int
+	start, dest coord
+)
+
+func initialize(in io.Reader) {
+	risk = make(map[coord]int)
+	scan := bufio.NewScanner(os.Stdin)
+	for scan.Scan() {
+		split := strings.Split(scan.Text(), "")
+		if cols == 0 {
+			cols = len(split)
+		}
+		for i, c := range split {
+			val, err := strconv.Atoi(c)
+			if err != nil {
+				log.Fatal(err)
+			}
+			risk[coord{rows, i}] = val
+		}
+		rows++
+	}
+	if err := scan.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	start, dest = coord{0, 0}, coord{rows - 1, cols - 1}
+
+	best = make(map[coord]int)
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			best[coord{i, j}] = math.MaxInt
+		}
+	}
+	best[start] = 0
+}
+
+func display(m map[coord]int) {
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			if j > 0 {
+				fmt.Print(" ")
+			}
+			fmt.Print(m[coord{i, j}])
+		}
+		fmt.Println()
+	}
+}
+
+type coord struct {
+	row, col int
+}
+
+func (c coord) valid() bool {
+	return c.row >= 0 && c.row < rows && c.col >= 0 && c.col < cols
+}
+
+func (c coord) sum(d coord) coord {
+	return coord{c.row + d.row, c.col + d.col}
+}
+
+func (c coord) String() string {
+	return fmt.Sprintf("(%d,%d)", c.row, c.col)
+}
+
+var directions = []coord{
+	{0, 1},  // right
+	{1, 0},  // down
+	{-1, 0}, // up
+	{0, -1}, // left
+}
+
+func main() {
+	initialize(os.Stdin)
+
+	queue := []coord{start}
+	for len(queue) > 0 {
+		cur := queue[0]
+		queue = queue[1:]
+
+		for _, dir := range directions {
+			next := cur.sum(dir)
+			if !next.valid() {
+				continue
+			}
+
+			if best[cur]+risk[next] < best[next] {
+				best[next] = best[cur] + risk[next]
+				queue = append(queue, next)
+			}
+		}
+	}
+
+	fmt.Println(best[dest])
+}
